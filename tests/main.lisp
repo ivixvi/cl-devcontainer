faeb16760a3ce8b775a4196d8cc32a68f6a76212
ksakai <epitus628@gmail.com>
2025-01-04 02:19:16 +0000

    wip

--- tests/main.lisp
+++ tests/main.lisp
@@ -4,6 +4,8 @@
         :greeting))
 (in-package :greeting/tests)
 
+(defun run-test () (rove:run :greeting/tests))
+
 (rove:deftest test-hello
   (testing "hello common lisp"
     (ok (string= (greeting:hello "common lisp") "Hello common lisp!"))))
