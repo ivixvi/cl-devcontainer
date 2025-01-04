613653e55e4a30bd36c59e4f9aa4755181370f5d
ksakai <epitus628@gmail.com>
2025-01-04 03:59:53 +0000

    ロード順？

--- tests/main.lisp
+++ tests/main.lisp
@@ -6,11 +6,11 @@
   (:export :run-test))
 (in-package :greeting/tests)
 
+(rove:deftest test-hello
+  (testing "hello common lisp"
+    (ok (string= (greeting:hello "common lisp") "Hello common lisp!f"))))
+
 (defun run-test ()
   (unless
     (rove:run :greeting/tests)
     (uiop:quit 1)))
-
-(rove:deftest test-hello
-  (testing "hello common lisp"
-    (ok (string= (greeting:hello "common lisp") "Hello common lisp!"))))
