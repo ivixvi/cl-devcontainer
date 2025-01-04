68458c90b905570051ae620617ccb9d5e83e2306
ksakai <epitus628@gmail.com>
2025-01-04 05:49:36 +0000

    remove: uiop

--- tests/main.lisp
+++ tests/main.lisp
@@ -1,22 +1,14 @@
 (defpackage :greeting/tests
   (:use :cl
         :rove
-        :uiop
         :greeting))
 (in-package :greeting/tests)
 
 (rove:deftest test-hello1
   (testing "hello common lisp"
-    (ok (string= (greeting:hello "common lisp") "Hello common lisp!")))
-   )
-
-(rove:deftest test-hello2
-(testing "ng case"
-    (ng (string= (greeting:hello "common lisp") "Hello world!")))
- )
-
+    (ok (string= (greeting:hello "common lisp") "Hello common lisp!"))))
 
 (defun run-test ()
   (unless
     (rove:run :greeting/tests)
-    (uiop:quit 1)))
+    (error "Failed tests.")))
